32a640e75452725e9cc005f4b8b2e72f01da07db
HarryHuangzhuang <harryhuang9527@gmail.com>
2024-09-05 19:29:55 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Layout from "../pages/Layout";
+import Login  from "@/pages/Login"
+
+import { createBrowserRouter } from 'react-router-dom';
+
+
+const router =  createBrowserRouter([
+    {
+        path:"/",
+        element  :<Layout/>
+
+    },
+    {
+        path:"/Login",
+        element  :<Login/>
+
+    },
+
+
+
+]
+)  
+
+export default router
